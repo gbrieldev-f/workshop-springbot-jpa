ab7dc1219f8decdbe2ead802961b4698532dfcbb
gabrielferreirainic <gabrielferreiradesousa06@gmail.com>
2026-05-18 16:16:51 -0300

    Project Created

---
+++ src/main/java/com/educandoweb/testeCourse/TesteCourseApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.testeCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteCourseApplication.class, args);
+	}
+
+}
