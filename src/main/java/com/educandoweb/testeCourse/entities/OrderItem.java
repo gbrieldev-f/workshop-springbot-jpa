9550bf52df44ffd15a89087deca2fbe3bb258cc0
gabrielferreirainic <gabrielferreiradesousa06@gmail.com>
2026-05-25 17:40:01 -0300

    OrderItem, many-to-many association with extra attributes pt.01

---
+++ src/main/java/com/educandoweb/testeCourse/entities/OrderItem.java
@@ -0,0 +1,71 @@
+package com.educandoweb.testeCourse.entities;
+
+import com.educandoweb.testeCourse.entities.pk.OrderItemPK;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+
+    @EmbeddedId
+    private OrderItemPK id;
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem(){}
+
+    public OrderItem(Order order, Product product, Double price, Integer quantity) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Order getOrder(){
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order){
+        id.setOrder(order);
+    }
+
+    public Product getProduct(){
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product){
+        id.setProduct(product);
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
