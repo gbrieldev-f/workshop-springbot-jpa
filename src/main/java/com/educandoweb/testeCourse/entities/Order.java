abf0679bc7386390743d575c6b41bce7cf76cf22
gabrielferreirainic <gabrielferreiradesousa06@gmail.com>
2026-05-25 19:30:33 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/testeCourse/entities/Order.java
+++ src/main/java/com/educandoweb/testeCourse/entities/Order.java
@@ -90,6 +90,14 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    public double getTotal(){
+        Double total = 0.0;
+        for(OrderItem orI : items){
+            total += orI.getSubTotal();
+        }
+        return total;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
