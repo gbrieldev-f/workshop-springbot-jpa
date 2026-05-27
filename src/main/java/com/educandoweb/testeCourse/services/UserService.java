eb0ccf37451a9a100791fa663d1ad63df61e1617
gabrielferreirainic <gabrielferreiradesousa06@gmail.com>
2026-05-27 18:00:03 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/testeCourse/services/UserService.java
+++ src/main/java/com/educandoweb/testeCourse/services/UserService.java
@@ -2,8 +2,11 @@ package com.educandoweb.testeCourse.services;
 
 import com.educandoweb.testeCourse.entities.User;
 import com.educandoweb.testeCourse.repositories.UserRepository;
+import com.educandoweb.testeCourse.services.exceptions.DatabaseException;
 import com.educandoweb.testeCourse.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(long id){
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch(EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch(DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User user) {
